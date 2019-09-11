054411a984adffd869bed988866d2621bbd68423
EvertonWalix <evertonwalixyt@gmail.com>
2019-09-11 15:28:32 -0300

    Contribuicao de Everton Walix, turma 524

--- calculadora.py
+++ calculadora.py
@@ -2,16 +2,22 @@
 
 def soma(a, b):
 	#Insira o codigo aqui
+    print(num1, "+", num2,"Resultado =", a+b)
 
 def subtrai(a, b):
 	#Insira o codigo aqui
+    print(num1, "-", num2, "Resultado =", a-b)
 
 def multiplica(a, b):
 	#Insira o codigo aqui
+    print(num1, "*", num2,"Resultado =", a*b)
 
 def divide(a, b):
 	#Insira o codigo aqui
-
+    if (b == 0):
+        print("Não se pode dividir por 0")
+    else:
+        print(num1, "/", num2,"Resultado =", a/b)
 
 #Programa principal
 
@@ -24,4 +30,5 @@ soma(num1, num2)
 subtrai(num1, num2)
 multiplica(num1, num2)
 divide(num1, num2)
+
 
